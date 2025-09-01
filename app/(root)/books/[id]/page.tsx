7e897bcc3e9deef119294dfca6d4b271de6dc234
Kelvin <kelvinkwasi.dev@gmail.com>
2025-09-01 11:13:19 +0000

    Temporarily disable book video to reduce imagekit costs

--- app/(root)/books/[id]/page.tsx
+++ app/(root)/books/[id]/page.tsx
@@ -2,7 +2,7 @@ import { db } from "@/app/database/drizzle";
 import { books } from "@/app/database/schema";
 import { auth } from "@/auth";
 import BookOverview from "@/components/BookOverview";
-import BookVideo from "@/components/BookVideo";
+//import BookVideo from "@/components/BookVideo";
 import { eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
 import React from "react";
@@ -31,7 +31,7 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
           <section className="flex flex-col gap-7">
             <h3>Video</h3>
 
-            <BookVideo videoUrl={bookDetails.videoUrl} />
+            {/* <BookVideo videoUrl={bookDetails.videoUrl} /> */}
           </section>
 
           <section className="mt-10 flex flex-col gap-7">
